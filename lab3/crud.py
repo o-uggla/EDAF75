511c539f4d7b06a3abcb55c70baaf158076faed1
Axel Voss <axelvoss96@gmail.com>
2020-02-15 23:16:48 +0100

    Minor fix

--- lab3/crud.py
+++ lab3/crud.py
@@ -4,35 +4,30 @@ import sqlite3
 class Database(object):
     def __init__(self, path: str):
         self.conn = sqlite3.connect(path, check_same_thread=False)
+        self.c = self.conn.cursor()
 
     def users(self):
         c = self.conn.cursor()
-        data = c.execute("SELECT * FROM users").fetchall()
+        data = self.c.execute("SELECT * FROM users").fetchall()
         return data
 
     def movies(self):
-        c = self.conn.cursor()
-        data = c.execute("SELECT * FROM movies").fetchall()
+        data = self.c.execute("SELECT * FROM movies").fetchall()
         return data
 
     def theaters(self):
-        c = self.conn.cursor()
-        data = c.execute("SELECT * FROM theaters").fetchall()
+        data = self.c.execute("SELECT * FROM theaters").fetchall()
         return data
 
     def performances(self):
-        c = self.conn.cursor()
-        data = c.execute("SELECT * FROM performances").fetchall()
+        data = self.c.execute("SELECT * FROM performances").fetchall()
         return data
 
     def tickets(self):
-        c = self.conn.cursor()
-        data = c.execute("SELECT * FROM tickets").fetchall()
+        data = self.c.execute("SELECT * FROM tickets").fetchall()
         return data
 
     def reset(self):
-        c = self.conn.cursor()
-
         users = [
             ('alice', 'Alice', 'dobido'),
             ('bob', 'Bob', 'whatsinaname')
@@ -51,15 +46,15 @@ class Database(object):
             ('Skandia', '100')
         ]
 
-        c.execute("DELETE FROM users")
-        c.executemany(
+        self.c.execute("DELETE FROM users")
+        self.c.executemany(
             "INSERT INTO users values (?,?,?)", users)
 
-        c.execute("DELETE FROM movies")
-        c.executemany(
+        self.c.execute("DELETE FROM movies")
+        self.c.executemany(
             "INSERT INTO movies values (?,?,?)", movies)
 
-        c.execute("DELETE FROM theaters")
-        c.executemany(
+        self.c.execute("DELETE FROM theaters")
+        self.c.executemany(
             "INSERT INTO theaters values (?,?)", theaters)
         self.conn.commit()
